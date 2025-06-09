73e5aa69eaee5248fcdc0ddce2e275c10c11c574
roman <roman@roman-deb.tyl.a7015.afu>
2025-06-09 19:26:29 +0300

    half

--- system/maps/100000/test.tcl
+++
@@ -1,6 +0,0 @@
-#!/bin/env tclsh
-
-foreach i [list 65 66 67 68 69 70 71 72] {
- puts m-36-0$i.jpg
- exec wget -nv https://freemap.com.ua/wp-content/themes/foundation_xy/assets/images/maps/genshtab/m-36-0$i.jpg
-}
